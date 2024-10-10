8cb3f77a8c265e68d3799d7a5156573bc31d0953
hax0sen <60272204+hax0sen@users.noreply.github.com>
2024-10-10 20:02:27 +0200

    f

--- db/example.count.severity.sql
+++
@@ -1,4 +0,0 @@
-SELECT vulnerability_data->>'severity' AS severity,
-       COUNT(*) AS count
-FROM grype_vulnerabilities_json
-GROUP BY vulnerability_data->>'severity';
--- db/example.critical.Vulnerabilities.sql
+++
@@ -1,6 +0,0 @@
-SELECT vulnerability_data->>'name' AS name,
-       vulnerability_data->>'version' AS version,
-       vulnerability_data->>'vulnerability_id' AS vulnerability_id,
-       vulnerability_data->>'severity' AS severity
-FROM grype_vulnerabilities_json
-WHERE vulnerability_data->>'severity' = 'Critical';
